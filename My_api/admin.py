8dd07f160866cdad20539580ece3debd75ff4feb
lxqiang95 <lxq13925731395@163.com>
2021-03-05 17:59:31 +0800

    django

--- My_api/admin.py
+++ My_api/admin.py
@@ -1,21 +1,21 @@
 from django.contrib import admin
-# Register your models here.
-from django.utils.module_loading import import_string
 
-from My_api import apps
 from My_api.models import *
+
+# Register your models here.
 
 # admin.site.register() 是注册用的函数
 
-admin.site.register(DB_Roast)
-admin.site.register(DB_home_href)
-admin.site.register(DB_project)
-admin.site.register(DB_apis)
-admin.site.register(DB_apis_log)
-admin.site.register(DB_cases)
-admin.site.register(DB_step)
-admin.site.register(DB_project_header)
-admin.site.register(DB_host)
-admin.site.register(DB_project_host)
-admin.site.register(DB_login)
-admin.site.register(DB_global_data)
+admin.site.register(DbApis)
+admin.site.register(DbApisLog)
+admin.site.register(DbCases)
+admin.site.register(DbStep)
+admin.site.register(DbGlobalData)
+admin.site.register(DbHomeHref)
+admin.site.register(DbHost)
+admin.site.register(DbLogin)
+admin.site.register(DbProject)
+admin.site.register(DbProjectHeader)
+admin.site.register(DbProjectHost)
+admin.site.register(DbRoast)
+admin.site.register(DbUser)
