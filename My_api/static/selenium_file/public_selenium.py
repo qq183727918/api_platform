d5ab6c5e6b24d2bf75db476f57388a2fe3bc4027
lxqiang95 <lxq13925731395@163.com>
2021-05-06 20:29:48 +0800

    selenium

--- My_api/static/selenium_file/public_selenium.py
+++ My_api/static/selenium_file/public_selenium.py
@@ -14,174 +14,150 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.wait import WebDriverWait
 
-driver = webdriver.Chrome()
-wait = WebDriverWait(driver, 60)
 
+class selenium_test:
 
-# name 定位
-def path_name_click(path):
-    """name 定位 点击"""
-    driver.find_element_by_name(path).click()
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+        self.wait = WebDriverWait(self.driver, 60)
 
+    # name 定位
+    def path_name_click(self, path):
+        """name 定位 点击"""
+        self.driver.find_element_by_name(path).click()
 
-# class 定位
-def path_class_click(path):
-    """class 定位 点击"""
-    driver.find_element_by_class_name(path).click()
+    # class 定位
+    def path_class_click(self, path):
+        """class 定位 点击"""
+        self.driver.find_element_by_class_name(path).click()
 
+    # tag 定位
+    def path_tag_click(self, path):
+        """tag 定位 点击"""
+        self.driver.find_element_by_tag_name(path).click()
 
-# tag 定位
-def path_tag_click(path):
-    """tag 定位 点击"""
-    driver.find_element_by_tag_name(path).click()
+    # link 定位
+    def path_link_click(self, path):
+        """link 定位 点击"""
+        self.driver.find_element_by_link_text(path).click()
 
+    # partial_link 定位
+    def path_partial_link_click(self, path):
+        """partial_link 定位 点击"""
+        self.driver.find_element_by_partial_link_text(path).click()
 
-# link 定位
-def path_link_click(path):
-    """link 定位 点击"""
-    driver.find_element_by_link_text(path).click()
+    # xpath 定位
+    def path_xpath_click(self, path):
+        """xpath 定位 点击"""
+        self.driver.find_element_by_xpath(path).click()
 
+    # CSS 定位
+    def path_css_click(self, path):
+        """CSS 定位 点击"""
+        self.driver.find_element_by_css_selector(path).click()
 
-# partial_link 定位
-def path_partial_link_click(path):
-    """partial_link 定位 点击"""
-    driver.find_element_by_partial_link_text(path).click()
+    # id 定位
+    def path_id_click(self, path):
+        """id 定位 点击"""
+        self.driver.find_element_by_id(path).click()
 
+    # name 定位
+    def path_name_send(self, path, values):
+        """name 定位 输入"""
+        self.driver.find_element_by_name(path).send_keys(values)
 
-# xpath 定位
-def path_xpath_click(path):
-    """xpath 定位 点击"""
-    driver.find_element_by_xpath(path).click()
+    # class 定位
+    def path_class_send(self, path, values):
+        """class 定位 输入"""
+        self.driver.find_element_by_class_name(path).send_keys(values)
 
+    # tag 定位
+    def path_tag_send(self, path, values):
+        """tag 定位 输入"""
+        self.driver.find_element_by_tag_name(path).send_keys(values)
 
-# CSS 定位
-def path_css_click(path):
-    """CSS 定位 点击"""
-    driver.find_element_by_css_selector(path).click()
+    # link 定位
+    def path_link_send(self, path, values):
+        """link 定位 输入"""
+        self.driver.find_element_by_link_text(path).send_keys(values)
 
+    # partial_link 定位
+    def path_partial_link_send(self, path, values):
+        """partial_link 定位 输入"""
+        self.driver.find_element_by_partial_link_text(path).send_keys(values)
 
-# id 定位
-def path_id_click(path):
-    """id 定位 点击"""
-    driver.find_element_by_id(path).click()
+    # xpath 定位
+    def path_xpath_send(self, path, values):
+        """xpath 定位 输入"""
+        self.driver.find_element_by_xpath(path).send_keys(values)
 
+    # CSS 定位
+    def path_css_send(self, path, values):
+        """CSS 定位 输入"""
+        self.driver.find_element_by_css_selector(path).send_keys(values)
 
-# name 定位
-def path_name_send(path, values):
-    """name 定位 输入"""
-    driver.find_element_by_name(path).send_keys(values)
+    # id 定位
+    def path_id_send(self, path, values):
+        """id 定位 输入"""
+        self.driver.find_element_by_id(path).send_keys(values)
 
+    # 显示等待
+    def wait_ec_xpath(self, path):
+        """显示等待wait_ec_xpath"""
+        self.wait.until(ec.presence_of_element_located((By.XPATH, path)))
 
-# class 定位
-def path_class_send(path, values):
-    """class 定位 输入"""
-    driver.find_element_by_class_name(path).send_keys(values)
+    # 显示等待
+    def wait_ec_id(self, path):
+        """显示等待 wait_ec_id"""
+        self.wait.until(ec.presence_of_element_located((By.ID, path)))
 
+    # 显示等待
+    def wait_ec_name(self, path):
+        """显示等待 wait_ec_name"""
+        self.wait.until(ec.presence_of_element_located((By.NAME, path)))
 
-# tag 定位
-def path_tag_send(path, values):
-    """tag 定位 输入"""
-    driver.find_element_by_tag_name(path).send_keys(values)
+    # 显示等待
+    def wait_ec_css_selector(self, path):
+        """显示等待 wait_ec_css_selector"""
+        self.wait.until(ec.presence_of_element_located((By.CSS_SELECTOR, path)))
 
+    # 显示等待
+    def wait_ec_link_text(self, path):
+        """显示等待 wait_ec_link_text"""
+        self.wait.until(ec.presence_of_element_located((By.LINK_TEXT, path)))
 
-# link 定位
-def path_link_send(path, values):
-    """link 定位 输入"""
-    driver.find_element_by_link_text(path).send_keys(values)
+    # 显示等待
+    def wait_ec_partial_link_text(self, path):
+        """显示等待 wait_ec_partial_link_text"""
+        self.wait.until(ec.presence_of_element_located((By.PARTIAL_LINK_TEXT, path)))
 
+    # 显示等待
+    def wait_ec_class_name(self, path):
+        """显示等待 wait_ec_class_name"""
+        self.wait.until(ec.presence_of_element_located((By.CLASS_NAME, path)))
 
-# partial_link 定位
-def path_partial_link_send(path, values):
-    """partial_link 定位 输入"""
-    driver.find_element_by_partial_link_text(path).send_keys(values)
+    # 显示等待
+    def wait_ec_tag_name(self, path):
+        """显示等待 wait_ec_tag_name"""
+        self.wait.until(ec.presence_of_element_located((By.TAG_NAME, path)))
 
+    # 强制等待
+    def wait_sleep(self):
+        """强制等待3秒"""
+        time.sleep(3)
 
-# xpath 定位
-def path_xpath_send(path, values):
-    """xpath 定位 输入"""
-    driver.find_element_by_xpath(path).send_keys(values)
+    # 切换进入iframe
+    def In_iframe(self, path_type):
+        """定位方式加路径 切换进入iframe"""
+        self.driver.switch_to.frame(path_type)
 
+    # 切换退出iframe
+    def Out_iframe(self):
+        """切换退出iframe"""
+        self.driver.switch_to_default_content()
 
-# CSS 定位
-def path_css_send(path, values):
-    """CSS 定位 输入"""
-    driver.find_element_by_css_selector(path).send_keys(values)
-
-
-# id 定位
-def path_id_send(path, values):
-    """id 定位 输入"""
-    driver.find_element_by_id(path).send_keys(values)
-
-
-# 显示等待
-def wait_ec_xpath(path):
-    """显示等待wait_ec_xpath"""
-    wait.until(ec.presence_of_element_located((By.XPATH, path)))
-
-
-# 显示等待
-def wait_ec_id(path):
-    """显示等待 wait_ec_id"""
-    wait.until(ec.presence_of_element_located((By.ID, path)))
-
-
-# 显示等待
-def wait_ec_name(path):
-    """显示等待 wait_ec_name"""
-    wait.until(ec.presence_of_element_located((By.NAME, path)))
-
-
-# 显示等待
-def wait_ec_css_selector(path):
-    """显示等待 wait_ec_css_selector"""
-    wait.until(ec.presence_of_element_located((By.CSS_SELECTOR, path)))
-
-
-# 显示等待
-def wait_ec_link_text(path):
-    """显示等待 wait_ec_link_text"""
-    wait.until(ec.presence_of_element_located((By.LINK_TEXT, path)))
-
-
-# 显示等待
-def wait_ec_partial_link_text(path):
-    """显示等待 wait_ec_partial_link_text"""
-    wait.until(ec.presence_of_element_located((By.PARTIAL_LINK_TEXT, path)))
-
-
-# 显示等待
-def wait_ec_class_name(path):
-    """显示等待 wait_ec_class_name"""
-    wait.until(ec.presence_of_element_located((By.CLASS_NAME, path)))
-
-
-# 显示等待
-def wait_ec_tag_name(path):
-    """显示等待 wait_ec_tag_name"""
-    wait.until(ec.presence_of_element_located((By.TAG_NAME, path)))
-
-
-# 强制等待
-def wait_sleep():
-    """强制等待3秒"""
-    time.sleep(3)
-
-
-# 切换进入iframe
-def In_iframe(path_type):
-    """定位方式加路径 切换进入iframe"""
-    driver.switch_to.frame(path_type)
-
-
-# 切换退出iframe
-def Out_iframe():
-    """切换退出iframe"""
-    driver.switch_to_default_content()
-
-
-# 鼠标悬停
-def Hovering(move):
-    """定位到悬停元素进行悬停"""
-    # 对定位到的元素执行悬停操作
-    ActionChains(driver).move_to_element(move).perform()
+    # 鼠标悬停
+    def Hovering(self, move):
+        """定位到悬停元素进行悬停"""
+        # 对定位到的元素执行悬停操作
+        ActionChains(self.driver).move_to_element(move).perform()
--- My_api/static/selenium_file/test.py
+++ My_api/static/selenium_file/test.py
@@ -12,7 +12,7 @@ from HTMLTestRunner1 import HTMLTestRunner
 
 from selenium import webdriver
 
-# from My_api.static.selenium_file.public_selenium import *
+from My_api.static.selenium_file.public_selenium import *
 
 
 class selenium_test(unittest.TestCase):
@@ -21,14 +21,13 @@ class selenium_test(unittest.TestCase):
         self.driver = webdriver.Chrome()
 
     def test(self):
-
         self.url = "http://www.baidu.com"
 
         self.driver.maximize_window()
 
         self.driver.get(self.url)
 
-        # path_id_send('kw', '这只是个测试')
+        path_id_send('kw', '这只是个测试')
 
         time.sleep(3)
 
@@ -36,23 +35,28 @@ class selenium_test(unittest.TestCase):
         self.driver.close()
 
 
-def run():
-    import unittest
-    import HTMLTestRunner1
+unittest.main()
 
-    file_path = r'..\\platform\\My_api\\templates\\Reports\\HtmlTest\\htmltesttrunner\\'
-
-    # 自动生成测试套件
-    suite = unittest.defaultTestLoader.discover("..\\platform\\My_api\\static\\selenium_file\\", pattern="test.py")
-
-    # 定义测试报告文件对象
-    file = open(fr'{file_path}\latest.html', 'wb')
-
-    # 生成运行器
-    runner = HTMLTestRunner1.HTMLTestRunner(file, title='TestReport', description='SEM_TestReport')
-
-    # 运行
-    runner.run(suite)
-
-    # 关闭文件
-    file.close()
+# def run():
+#     import unittest
+#     import HTMLTestRunner1
+#
+#     file_path = r'..\\platform\\My_api\\templates\\Reports\\HtmlTest\\htmltesttrunner\\'
+#
+#     # 自动生成测试套件
+#     suite = unittest.defaultTestLoader.discover("..\\platform\\My_api\\static\\selenium_file", pattern="test.py")
+#
+#     # 定义测试报告文件对象
+#     file = open(fr'{file_path}\latest.html', 'wb')
+#
+#     # 生成运行器
+#     runner = HTMLTestRunner1.HTMLTestRunner(file, title='TestReport', description='SEM_TestReport')
+#
+#     # 运行
+#     runner.run(suite)
+#
+#     # 关闭文件
+#     file.close()
+#
+#
+# run()
